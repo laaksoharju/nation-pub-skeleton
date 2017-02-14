788dfc08639c29fe5dbf1c6dd68630ad83924137
Mikael Laaksoharju <mikael.laaksoharju@it.uu.se>
2017-02-14 01:31:17 +0100

    testing a bit more complex structures

--- public/js/diner.js
+++ public/js/diner.js
@@ -16,15 +16,27 @@ function getOrderNumber() {
 new Vue({
   el: '#ordering',
   mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
+  data: {
+    mainDish: [],
+    extras: []
+  },
   methods: {
     placeOrder: function() {
-      // create an array of checked items to order
-    var orderItems = [].filter.call(document.getElementsByName('item[]'), function(i) {
-      return i.checked;
-    }).map(function(i) {
-      return i.value;
-    });
+      // Here two ways of getting selected items are illustrated
+      // 1. The Vue way, notice the data model declarations above
+      var mainCourse = this.mainDish;
+      var extras = this.extras;
+      // 2. The old-school way: create an array with values of checked items
+      var theRest = [].filter.call(document.getElementsByName('item[]'), function(i) {
+        return i.checked;
+      }).map(function(i) {
+        return i.value;
+      });
+      // OK, it's not really neat to use two different ways of accomplishing the same thing
+      // but let's pretend it's for an educational purpose ... here comes another no-no:
+      var orderItems = mainCourse.concat(extras).concat(theRest);
+      // Finally we make use of socket.io's magic to send the stuff to the kitchen
       socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
-  }
+    }
   }
 });
